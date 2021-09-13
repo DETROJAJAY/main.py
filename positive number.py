a3f0f4faf0151cc822f3b0856356b7a870ca647b
DETROJAJAY <90523002+DETROJAJAY@users.noreply.github.com>
2021-09-13 08:58:30 +0545

    print positive number in given range
    hey bro, I am jay, and here is the program to write a positive number in a given range by the user.

---
+++ positive number.py
@@ -0,0 +1,9 @@
+start = int(input("enter a start number"))
+end = int(input("enter the last number"))
+for b in range(start,end +1 ):
+        if b>0:
+            print("positive number is: " ,int(b))
+
+
+        
+ 
